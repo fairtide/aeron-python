b289412426ea67445f4c4494d08334cfbc10ef8f
Lukasz Laszko <lukasz@fairtide.co>
2018-11-07 08:48:34 +0800

    Support integrated aeron python driver during tests added.

--- sources/package/aeronpy/__init__.py
+++ sources/package/aeronpy/__init__.py
@@ -4,3 +4,6 @@ from .data import *
 from .exclusive_publication import *
 from .publication import *
 from .subscription import *
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
